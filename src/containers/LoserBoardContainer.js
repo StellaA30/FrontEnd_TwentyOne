71fa8156e4e00c527aae6f4fd4cd10d1baf9a6aa
Your Name <you@example.com>
2023-05-26 12:50:09 +0100

    Fetch loserboard

--- src/containers/LoserBoardContainer.js
+++ src/containers/LoserBoardContainer.js
@@ -1,8 +1,28 @@
+import { useState, useEffect} from "react";
+
 const LooserBoardContainer = () => {
+    const[loserBoard,setLoserBoard] = useState([]);
+
+    useEffect(() => {
+        const fetchLoserBoard = async () => {
+            const response = await fetch("http://localhost:8080/loserBoard")
+            const jsonData = await response.json();
+            setLoserBoard(jsonData);
+        }
+        fetchLoserBoard()
+    }, [])
+
+    const loserBoardItems = loserBoard.map((player) => { 
+        return <li>{player} </li>
+    })
+
     return ( 
         <>
         
          <h3> Hello from loserboard!!</h3>
+         <ol>
+            {loserBoardItems}
+         </ol>
         
         </>
      );
