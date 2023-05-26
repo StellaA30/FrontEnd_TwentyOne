2b1c2af4dffa11f083dc61edfe02fbcb5b12d892
Zaynah <zaynah.sadiq@hotmail.com>
2023-05-26 12:23:10 +0100

    removed comments

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
     <BrowserRouter>
     <div className="App">
       <h1>Hello BNTA World</h1>
-      <p> Hiii guysssss its meeeeee</p>
       <Routes>
         <Route path='/singlePlayer' element={<SinglePlayerContainer/>} />
         <Route path='/MultiPlayer' element={<MultiPlayerContainer/>} />
