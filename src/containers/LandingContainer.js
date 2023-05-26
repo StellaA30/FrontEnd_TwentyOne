26e01ffb0a0c8b646289f7925a9df9325b88e0e4
Zaynah <zaynah.sadiq@hotmail.com>
2023-05-26 15:59:41 +0100

    added to landingContainer

--- src/containers/LandingContainer.js
+++ src/containers/LandingContainer.js
@@ -1,17 +1,38 @@
-const LandingContainer = () => {
-    return ( 
-        <>
+import { useState } from "react";
 
-            <h3>Hello from Landing Page </h3>
-            <h4>How to play:</h4>
-            <select  name = "game mode" value = "game mode">
-                <option disabled-value="Game mode">Game mode</option>
-                
-                </select>
-        
-        
-        </>
-     );
-}
- 
+const LandingContainer = ({singlePlayerRoute}) => {
+
+    const [selectedMode, setSelectedMode] = useState(null)
+
+    const handleGameMode = (event) => {
+        const selectedMode = event.target[event.target.selectedIndex].text;
+        setSelectedMode(selectedMode);
+};
+   
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+
+        if (selectedMode=== "Single") {
+            singlePlayerRoute()
+        }
+};
+
+  return (
+    <>
+      <h3>Hello from Landing Page </h3>
+      <h4>How to play:</h4>
+      <p>Write rules in here:</p>
+      <form onSubmit={handleFormSubmit}>
+      <select onChange={handleGameMode}  name="game mode" defaultValue="game mode">
+        <option disabled-value="game mode">Game mode</option>
+        <option value="single">Single</option>
+        <option value="multiplayer">Multiplayer</option>
+      </select>
+      <button type ="submit">Submit</button>
+      </form>
+    </>
+  );
+};
+
 export default LandingContainer;
