55c6a9c1d1de3963380b9fba344c009edeb1b216
Zaynah <zaynah.sadiq@hotmail.com>
2023-05-26 12:37:14 +0100

    added a drop down menu

--- src/containers/LandingContainer.js
+++ src/containers/LandingContainer.js
@@ -3,6 +3,11 @@ const LandingContainer = () => {
         <>
 
             <h3>Hello from Landing Page </h3>
+            <h4>How to play:</h4>
+            <select  name = "game mode" value = "game mode">
+                <option disabled-value="Game mode">Game mode</option>
+                
+                </select>
         
         
         </>
