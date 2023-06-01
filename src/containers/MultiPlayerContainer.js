54542b6df92813d63bf5b94ab611b502e8503a75
Stella Annor <stella.annor@icloud.com>
2023-06-01 11:30:18 +0100

    adding select options for multiplayer

--- src/containers/MultiPlayerContainer.js
+++ src/containers/MultiPlayerContainer.js
@@ -1,4 +1,7 @@
 import PlayerComponent from "../components/PlayerComponent"
+import Select from "react-select";
+import { useState } from "react";
+
 
 const MultiPlayerContainer = ({leadPlayer, activePlayer}) => {
 
@@ -6,26 +9,43 @@ const MultiPlayerContainer = ({leadPlayer, activePlayer}) => {
     // for loop and if statement if activeplayer.id and lead.is match 
     // splice out that person 
 
-    const getAllOtherPlayers = activePlayer.filter(player => player.id !== leadPlayer.id).map((player) => {
-        return(
-            <option key={player.id} value={player.name}>
-          {player.name}
-        </option>
-        )
-    })
+    // const getAllOtherPlayers = activePlayer.filter(player => player.id !== leadPlayer.id).map((player) => {
+    //     return(
+    //         <option key={player.id} value={player.name}>
+    //       {player.name}
+    //     </option>
+    //     )
+    // })
+
+
+    const getAllOtherPlayers = activePlayer.filter(player => player.id !== leadPlayer.id).map(player => player.name)
+
+    const [selectedTags, setSelectedTags] = useState([]);
+
+    const onChange = (selectedOptions) => {setSelectedTags(selectedOptions)};
 
     return ( 
     <>
 
         <h3> Hi {leadPlayer.name} </h3>
         <p> all players must have an account </p>
-        <form>
+    <form className="">
+        <label className="">Related technologies</label>
+        <Select
+          options={getAllOtherPlayers.map((player) => ({ value: player, label: player }))}
+          value={selectedTags}
+          onChange={onChange}
+          isMulti
+          closeMenuOnSelect={false}
+        />
+      </form>
+        {/* <form>
         <select>
         {getAllOtherPlayers}
         </select>
         <button type="submit"> + </button>
 
-        </form>
+        </form> */}
     </> 
     );
 }
